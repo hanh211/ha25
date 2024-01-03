5456fad25bd3812126ee832b674d47d1e851da6f
hanh <hanh@gmail.com>
2024-01-03 10:18:07 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MainApp from './MainApp';
+import counterReducer from './reducers/CounterReducer';
 import reportWebVitals from './reportWebVitals';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+const store=configureStore({
+  reducer: {
+    "counter":counterReducer
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <MainApp />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
